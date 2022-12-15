b28e3de108c976c8bb2d5479d044ee425a6d0b39
chmanie <code@chmanie.com>
2022-12-15 18:14:53 +0900

    Add connect-metamask component

---
+++ src/components/ConnectMetaMask/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const addColonyRPC = () => {
+  // If MetaMask is installed there will be an `ethereum` object on the `window`
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).ethereum.request({
+    method: 'wallet_addEthereumChain',
+    params: [
+      {
+        chainId: '0x64',
+        chainName: 'Gnosis Chain via Colony',
+        nativeCurrency: {
+          name: 'xDAI',
+          symbol: 'XDAI',
+          decimals: 18,
+        },
+        rpcUrls: ['https://xdai.colony.io/rpc2/'],
+        blockExplorerUrls: ['https://blockscout.com/xdai/mainnet'],
+      },
+    ],
+  });
+};
+
+export default function ConnectMetaMask(): JSX.Element {
+  if ('ethereum' in window) {
+    return (
+      <button className={styles.button} onClick={addColonyRPC}>
+        <img src="/img/metamask-fox.svg" />
+        Add Gnosis Chain to MetaMask
+      </button>
+    );
+  }
+  return null;
+}
