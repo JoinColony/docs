c5f42cb1ece4d61e679700f0ae93d3e639d5c938
chmanie <code@chmanie.com>
2022-12-21 17:06:25 +0900

    Fix Colony SDK link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/colonysdk/start">
+            to="/colonysdk/getting-started">
             Start building now 🏗
           </Link>
         </div>
