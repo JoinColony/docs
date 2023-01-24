8dfe6f96c350fed49f173f31479bd521ebdba266
chmanie <code@chmanie.com>
2023-01-24 18:06:31 +0700

    Add "Join our Discord" button

---
+++ src/components/DiscordButton/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export default function ConnectMetaMask(): JSX.Element {
+  return (
+    <a className={styles.button} href="https://discord.gg/feVZWwysqM" target="_blank" rel="nofollow noreferrer noopener">
+      <img src="/img/discord-mark-white.svg" />
+      Join our Discord
+    </a>
+  );
+}
--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -1,9 +1,11 @@
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 import ConnectMetaMask from '@site/src/components/ConnectMetaMask';
+import DiscordButton from '@site/src/components/DiscordButton';
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   'connect-metamask': ConnectMetaMask,
+  'join-discord': DiscordButton,
 };
