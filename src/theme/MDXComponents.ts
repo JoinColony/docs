d51e1dfc3cb1b178eddc282021c8bbf8f8e0afc9
chmanie <code@chmanie.com>
2023-09-05 21:55:30 +0200

    Add Reputation Mining Register component

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -2,10 +2,14 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import ConnectMetaMask from '@site/src/components/ConnectMetaMask';
 import DiscordButton from '@site/src/components/DiscordButton';
+import RegisterReputationMining from '@site/src/components/RegisterReputationMining';
 
-export default {
+const components = {
   // Re-use the default mapping
   ...MDXComponents,
   'connect-metamask': ConnectMetaMask,
   'join-discord': DiscordButton,
+  'register-reputation-mining': RegisterReputationMining,
 };
+
+export default components;
