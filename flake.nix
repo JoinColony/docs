a23261bbff47ccde8daf05da37fd0f01e45c475d
Chris Maniewski <code@chmanie.com>
2024-03-12 20:19:43 +0100

    feat: move to pnpm

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Flake to develop the CDapp using nix(OS)";
+
+  inputs = {
+    # node 20.11.0
+    nixpkgs_node.url = "github:NixOS/nixpkgs/97b17f32362e475016f942bbdfda4a4a72a8a652";
+  };
+
+  outputs = {
+    self,
+    nixpkgs_node
+  }: {
+    devShell.x86_64-linux = with nixpkgs_node.legacyPackages.x86_64-linux;
+      mkShell {
+        buildInputs = [nodejs_20 nodePackages.pnpm zsh];
+        shellHook = ''
+          if [ -n "$SHELL" ]; then
+            exec $SHELL
+          fi
+        '';
+      };
+  };
+}
